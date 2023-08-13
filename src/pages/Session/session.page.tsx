10f6977858d80a14694fe4a514a905e70156d38f
rrodcastro <mrcastro9574@gmail.com>
2023-08-13 18:23:07 +0200

    Added audio session feature

--- src/pages/Session/session.page.tsx
+++ src/pages/Session/session.page.tsx
@@ -2,9 +2,9 @@ import { Box, Button, Typography } from "@mui/material";
 import BackComponent from "../../components/back/back.component";
 import { useSelector } from "react-redux";
 import { RootState } from "../../services/store";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useGetActivityQuery, useLazyGetActivityQuery, useLazyPostActivityQuery } from "../../services/activity.api";
+import { useLazyGetActivityQuery, useLazyPostActivityQuery } from "../../services/activity.api";
 import { DateTime } from "luxon";
 
 const SessionPage = () => {
@@ -18,6 +18,10 @@ const SessionPage = () => {
     const token = useSelector((state: RootState) => state.auth.token);
     const userId = useSelector((state: RootState) => state.auth.userId);
 
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+    const [progressPercentage, setProgressPercentage] = useState<number>(0);
+    const [startSession, setStartSession] = useState<boolean>(false);
+
     useEffect(() => {
         if (!session) {
             setTimeout(() => navigate("/home"), 1500);
@@ -26,13 +30,25 @@ const SessionPage = () => {
 
     const handleStartSession = async () => {
         if (session) {
-            const now = DateTime.now();
 
-            await postActivty({ token: token, body: { userId: userId, title: session.title, type: 1, date: now.toUTC().toString(), extra: { duration: `${session.duration.split(":")[0]} min` } } });
-            await getActivity({ token: token, userId: userId });
+            setStartSession(true);
         }
 
     }
+
+    const handleTimeUpdate = () => {
+        const audioElement = audioRef.current;
+        if (audioElement) {
+            const currentTime = audioElement.currentTime;
+            const duration = audioElement.duration;
+
+            if (duration) {
+                const percentage = (currentTime / duration) * 100;
+                setProgressPercentage(percentage);
+            }
+        }
+    };
+
     if (!session) {
         return (
             <Box className="session-container">
@@ -46,6 +62,15 @@ const SessionPage = () => {
         );
     }
 
+    const finishSession =  async () => {
+        const now = DateTime.now();
+
+        await postActivty({ token: token, body: { userId: userId, title: session.title, type: 1, date: now.toUTC().toString(), extra: { duration: `${session.duration.split(":")[0]} min` } } });
+        await getActivity({ token: token, userId: userId });
+        navigate(-1);
+    }
+
+    const isMediaEnded = progressPercentage > 92;
     return (
         <Box className="session-container">
             <BackComponent />
@@ -58,29 +83,54 @@ const SessionPage = () => {
                 component="img"
                 src={session.image}
             />
-            <Typography
-                className="session-description"
-            >
+            {startSession ? (
+                <Box>
 
-                {session.description}
-            </Typography>
-            <Button
-                style={{ marginTop: '24px' }}
-                variant="contained"
-                color="primary"
-                className="session-button start-session-button"
-                onClick={handleStartSession}
-            >
-                Start Session
-            </Button>
-            <Button
-                style={{ marginTop: '24px' }}
-                variant="contained"
-                color="primary"
-                className="session-button create-new-entry-button"
-            >
-                Create New Entry
-            </Button>
+                    <audio ref={audioRef} controls autoPlay onTimeUpdate={handleTimeUpdate}>
+                        <source src={session.resource} type="audio/mpeg" />
+                        Your browser does not support the audio element.
+                    </audio>
+
+                    <Button
+                        style={{ marginTop: '24px' }}
+                        variant="contained"
+                        color="primary"
+                        className="session-button start-session-button"
+                        onClick={() => isMediaEnded ? finishSession() : navigate(-1)}
+                    >
+                        {isMediaEnded ? 'Finish' : 'Cancel'}
+                    </Button>
+                    {
+                        isMediaEnded && <Button
+                            style={{ marginTop: '24px' }}
+                            variant="contained"
+                            color="primary"
+                            className="session-button create-new-entry-button"
+                            onClick={() => navigate("/new-entry")}
+                        >
+                            Create New Entry
+                        </Button>
+                    }
+                </Box>
+            ) : (
+                <Box> <Typography
+                    className="session-description"
+                >
+
+                    {session.description}
+                </Typography>
+                    <Button
+                        style={{ marginTop: '24px' }}
+                        variant="contained"
+                        color="primary"
+                        className="session-button start-session-button"
+                        onClick={handleStartSession}
+                    >
+                        Start Session
+                    </Button>
+
+                </Box>
+            )}
         </Box>
     )
 }
